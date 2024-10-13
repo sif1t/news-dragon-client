2747939bfdbdf603a11aa1578c88df96ae639a51
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-10-13 20:43:45 +0600

    implement News details display with Card component; add loader for fetching news by ID

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const News = () => {
+    const news = useLoaderData();
+    const { _id, title, details, image_url, category_id } = news;
+
     return (
-        <div>
-            <h2>News Details Coming son</h2>
-        </div>
+           <Card>
+      <Card.Img variant="top" src={image_url} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+            {details}
+        </Card.Text>
+        <Link to={`/category/${category_id}`}>
+        <Button variant="danger"><FaArrowLeft />
+        All News In This Category</Button></Link>
+      </Card.Body>
+    </Card>
     );
 };
 
