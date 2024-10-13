f2f4edf823ce3a557ea377f7b2f293dc1e527c52
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-10-13 21:10:39 +0600

    add EditorsInsights component to display additional news insights; integrate it into News page layout

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
+import Editorsinsights from '../EditorsInsights/Editorsinsights';
 
 const News = () => {
     const news = useLoaderData();
     const { _id, title, details, image_url, category_id } = news;
 
     return (
-           <Card>
-      <Card.Img variant="top" src={image_url} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-            {details}
-        </Card.Text>
-        <Link to={`/category/${category_id}`}>
-        <Button variant="danger"><FaArrowLeft />
-        All News In This Category</Button></Link>
-      </Card.Body>
-    </Card>
+       <div>
+          <Card>
+            <Card.Img variant="top" src={image_url} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {details}
+                </Card.Text>
+                <Link to={`/category/${category_id}`}>
+                    <Button variant="danger"><FaArrowLeft />
+                        All News In This Category</Button></Link>
+            </Card.Body>
+        </Card>
+        <Editorsinsights></Editorsinsights>
+       </div>
     );
 };
 
