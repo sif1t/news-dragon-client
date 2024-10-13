79a44fc67ea0ae39a49e072502fd5047f01c3282
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-10-13 20:23:05 +0600

    add react-rating package and enhance NewsCard component; display rating and total views

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -2,27 +2,55 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
+import moment from 'moment';
+import { IoBookmarkOutline } from 'react-icons/io5';
+import { IoShareSocialOutline } from 'react-icons/io5';
+import { FaRegEye } from 'react-icons/fa';
+import Rating from 'react-rating';
+import { FaRegStar } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 
 const NewsCard = ({ news }) => {
-    const {_id, title, details, image_url, author} = news;
+    const { _id, title, details, image_url, author, rating, total_view } = news;
     return (
         <Card className="mb-5">
-            <Card.Header>
-            <Image style={{height: '40px'}} src={author.img} roundedCircle  />
+            <Card.Header className='d-flex align-items-center'>
+                <Image style={{ height: '40px' }} src={author.img} roundedCircle />
+                <div className='ps-2 flex-grow-1'>
+                    <p className='m-0'>{author?.name}</p>
+                    <p><small>{moment(author?.published_date).format('YYYY-MM-D')}</small></p>
+                </div>
+                <div>
+                    <IoBookmarkOutline></IoBookmarkOutline>
+                    <IoShareSocialOutline></IoShareSocialOutline>
+                </div>
             </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Img variant="top" src={image_url} />
                 <Card.Text>
                     {details.length < 250 ? <>{details}</> :
-                       <>{details.slice(0, 250)}.... <Link to={`/news/${_id}`}>Read More</Link></>
+                        <>{details.slice(0, 250)}.... <Link to={`/news/${_id}`}>Read More</Link></>
                     }
                 </Card.Text>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted d-flex">
+                <div className='flex-grow-1'>
+                    <Rating
+                        placeholderRating={rating.number}
+                        readonly
+                        emptySymbol={<FaRegStar />}
+                        placeholderSymbol={<FaStar className='text-warning' />}
+                        fullSymbol={<FaStar />}
+                    />
+                    <span>{rating?.number}</span>
+                </div>
+                <div>
+                    <FaRegEye /> {total_view}
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
 
 export default NewsCard;
- 
