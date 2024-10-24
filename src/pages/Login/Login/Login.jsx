da16f96cfe6266d268d3f257d2f145dc3a74967d
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-10-24 20:04:22 +0600

    add login functionality; implement handleLogin in Login component; integrate AuthContext for user authentication

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,36 +1,52 @@
-import React from 'react';
-import { Container, Form , Button} from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const Login = () => {
+    const { signInUser } = useContext(AuthContext);
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signInUser (email, password) 
+        .then (result => {
+            const loggedInUser = result.user;
+            console.log (loggedInUser);
+        })
+        .catch (error => {
+            console.log (error);
+        })
+    }
     return (
         <Container className='w-25 mx-auto'>
             <h3>Please Login</h3>
-            <Form>
+            <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='name' placeholder="Enter email" required />
-                    
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-               <Form.Text></Form.Text>
+                <Form.Text></Form.Text>
                 <Button className='w-100' variant="primary" type="submit">
                     Login
                 </Button>
                 <br />
                 <Form.Text className="text-secondary">
-                     Don't Have an account  <Link to='/register'>Register</Link>
-                    </Form.Text>
+                    Don't Have an account  <Link to='/register'>Register</Link>
+                </Form.Text>
                 <Form.Text className="text-danger">
-                       
-                    </Form.Text>
+
+                </Form.Text>
             </Form>
         </Container>
     );
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -12,9 +12,15 @@ const AuthProvider = ({children}) => {
  const creteUser = (email, password) => {
      return  createUserWithEmailAndPassword (auth, email, password);
  }
+
+
+ const signInUser  = (email, password) => {
+      return signInWithEmailAndPassword (auth, email, password);
+ }
     const authInfo  = {
         user,
-        creteUser
+        creteUser,
+        signInUser
     };
 
     return (
