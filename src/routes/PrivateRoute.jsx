8cef18fde033f3a23ce978487f67a3678411f51b
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-10-26 18:13:32 +0600

    add PrivateRoute component; implement user authentication check and redirect logic

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PrivateRoute = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrivateRoute;
+
+
+/*
+------------------
+steps
+------------------
+1. check user is logged in or not
+2. if user is logged in, them to visit the route 
+3. Else redirect to login page
+*/
