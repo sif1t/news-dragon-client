b1fe755632e47dcfec3224d95ab66001fd6b4dee
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-10-28 20:19:24 +0600

    refactor Firebase configuration; correct property names and add hosting setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 console.log ('environment variable', import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKeyZ:import.meta.env.VITE_APIKEY,
-  authDomainZ: import.meta.env.VITE_AUTHDOMAIN,
-  projectIdZ: import.meta.env.VITE_PROJECTID,
-  storageBucketZ:import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderIdZ:import.meta.env.VITE_MESSAGINGSENDERID,
-  appIdZ:import.meta.env.VITE_APPID
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
